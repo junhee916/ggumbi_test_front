312da1d4a017cfc212f73b49c4b8f886a9261bdb
DESKTOP-5JUD91T\junhee <junhee916@naver.com>
2024-05-22 20:42:57 +0900

    ecosystem update -2

--- client/src/ecosystem.config.cjs
+++ client/src/ecosystem.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "react-app",
       script: "npx",
-      args: "serve -s build -l 3000",
-      cwd: "/home/ubuntu/ggumbi_test_front/client", // 프로젝트 디렉토리 경로
+      args: ["serve", "-s", "build", "-l", "3000"],
+      cwd: "/home/ubuntu/ggumbi_test_front/client", // 프로젝트 디렉토리 경로 수정
       env: {
         NODE_ENV: "production",
       },
