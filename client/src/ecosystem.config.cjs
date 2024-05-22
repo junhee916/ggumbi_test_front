5a156a5eb22db764b417591387ae227eea1ded1e
DESKTOP-5JUD91T\junhee <junhee916@naver.com>
2024-05-22 18:52:46 +0900

    ecosystem update -3

--- client/src/ecosystem.config.cjs
+++ client/src/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "react-app",
       script: "serve",
-      args: ["-s", "build", "-l", "3000"],
+      args: ["-s", "/home/ubuntu/ggumbi_test_front/client/build", "-l", "3000"],
       env: {
         PM2_SERVE_PATH: "/home/ubuntu/ggumbi_test_front/client/build",
         PM2_SERVE_PORT: 3000,
